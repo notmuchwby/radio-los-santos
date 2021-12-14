e46ddf3943968182275dbf1b95d8865c8c1f940b
notmuchwby <yamakasy1337@gmail.com>
2021-12-15 02:28:38 +0600

    made some styling changes with fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import Navbar from "./modules/Navbar";
 import Logo from "./modules/Logo";
 import WelcomeText from "./modules/WelcomeText";
-import Slider from "./modules/Slider";
 import './App.css'
 import './modules/main.css'
 
 function App() {
   return (
-    <div >
+    <div className="background-slides">
       <Navbar />
-
-      <main>
-        <Logo />
-        <WelcomeText />
-        {/* <Slider /> */}
-
-
-
-      </main>
+      <Logo />
+      <WelcomeText />
     </div>
   );
 }
